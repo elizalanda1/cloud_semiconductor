ad3474a66bc8dc8d3d67b71ab80f0f4fcc720e7f
chuyAguilar <jesusaguilar32265@gmail.com>
2024-10-30 13:44:23 +0900

    corregi problema en git con el codigo de la caminadora

--- Caminadora/Escanear.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import BleakScanner
-
-async def run():
-    devices = await BleakScanner.discover()
-    for device in devices:
-        print(device)
-
-asyncio.run(run())
