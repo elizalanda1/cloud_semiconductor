e1f0bd98853d3723173d034f5250f7d1d155d4b1
chuyAguilar <jesusaguilar32265@gmail.com>
2024-11-29 12:13:27 +0900

    borrando codigos sobrantes/pruebas

--- IsisBack/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-// Login Route
-router.post('/login', loginUser);
-// Register Route
-router.post('/register', registerUser);
-
-export default router;
